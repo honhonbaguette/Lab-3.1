1a3c7b3c9d500779034f46e4b9452ebeb6e26f45
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2018-02-01 09:23:13 -0500

    fixed

--- Lab.3.1/src/arrayMethods/runner.java
+++ Lab.3.1/src/arrayMethods/runner.java
@@ -10,29 +10,29 @@ public class runner
 		double[] test2 = {1.1, 4.1, 4.2, 5.0, 2.0, 4.0, 17.0, 0.0};
 		String[] test3 = {"zebra", "tortilla", "abba", "foo", "bar", "aba"};
 		
-		//Insertion Sort Test
+		//Insertion Sort 
 		long start= System.nanoTime();
 		SortingAlgorithm.insertionSort(test1);
 		long end = System.nanoTime();
 		long time = end - start;
 		System.out.println("Test1 took: " + time + "nanoseconds");
-		System.out.println(Arrays.toString(test1));
-		
-		//Selection Sort Test
+		//Selection Sort 
 		start = System.nanoTime();
 		SortingAlgorithm.selectionSort(test2);
 		end = System.nanoTime();
 		time = end - start;
 		System.out.println("Test2 took: " + time + "nanoseconds");
-		System.out.println(Arrays.toString(test2));
-		
-		//Bubble Sort Test
+		//Bubble Sort 
 		start = System.nanoTime();
 		SortingAlgorithm.bubbleSort(test3);
 		end = System.nanoTime();
 		time = end - start;
-		System.out.println("Test3 took: " + time + "nanoseconds");
+		System.out.println("Test3 took: " + time + "nanoseconds");		
+		
+		System.out.println(Arrays.toString(test1));
+		System.out.println(Arrays.toString(test2));
 		System.out.println(Arrays.toString(test3));
+		
 	}
 
 }
